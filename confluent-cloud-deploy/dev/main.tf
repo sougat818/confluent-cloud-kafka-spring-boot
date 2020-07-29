53664011390378a4aa0ad0a0d01cdbfa3d85bec5
Sougata Khan <sougat818@gmail.com>
2020-07-29 16:56:28 +1000

    tags

--- confluent-cloud-deploy/dev/main.tf
+++ confluent-cloud-deploy/dev/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+variable "default_tags" {
+  default = {
+    Cost = "Confluent"
+  }
+  description = "default resource tags"
+  type        = map(string)
+}
+
 #import -config=/workspace/confluent-cloud-deploy/dev  aws_s3_bucket.terraform_state sougat818-confluent-cloud-aws-terraform-state
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "sougat818-confluent-cloud-aws-terraform-state"
@@ -15,6 +23,12 @@ resource "aws_s3_bucket" "terraform_state" {
       }
     }
   }
+  tags = merge(
+    var.default_tags,
+    {
+      Env = "dev"
+    },
+  )
 }
 
 #import -config=/workspace/confluent-cloud-deploy/dev  aws_dynamodb_table.terraform_locks sougat818-confluent-cloud-aws-terraform-state-locks
@@ -26,15 +40,22 @@ resource "aws_dynamodb_table" "terraform_locks" {
     name = "LockID"
     type = "S"
   }
+  tags = merge(
+    var.default_tags,
+    {
+      Env = "dev"
+    },
+  )
 }
 
 #import -config=/workspace/confluent-cloud-deploy/dev aws_secretsmanager_secret.confluent_cloud arn:aws:secretsmanager:ap-southeast-2:439904798018:secret:confluent_cloud-HPCUM6
 
 resource "aws_secretsmanager_secret" "confluent_cloud" {
-  name = "confluent_cloud"
+  name                    = "confluent_cloud"
   recovery_window_in_days = 30
-  description = "key and secret for confluent cloud with cluster creation access"
+  description             = "key and secret for confluent cloud with cluster creation access"
 }
+
 
 data "aws_secretsmanager_secret_version" "confluent_cloud" {
   secret_id = "${aws_secretsmanager_secret.confluent_cloud.id}"
@@ -42,9 +63,9 @@ data "aws_secretsmanager_secret_version" "confluent_cloud" {
 
 terraform {
   backend "s3" {
-    bucket         = "sougat818-confluent-cloud-aws-terraform-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "ap-southeast-2"
+    bucket = "sougat818-confluent-cloud-aws-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "ap-southeast-2"
     # Replace this with your DynamoDB table name!
     dynamodb_table = "sougat818-confluent-cloud-aws-terraform-state-locks"
     encrypt        = true
@@ -52,8 +73,8 @@ terraform {
 }
 
 provider "confluentcloud" {
-    username = jsondecode(data.aws_secretsmanager_secret_version.confluent_cloud.secret_string)["confluent_cloud_key"]
-    password = jsondecode(data.aws_secretsmanager_secret_version.confluent_cloud.secret_string)["confluent_cloud_secret"]
+  username = jsondecode(data.aws_secretsmanager_secret_version.confluent_cloud.secret_string)["confluent_cloud_key"]
+  password = jsondecode(data.aws_secretsmanager_secret_version.confluent_cloud.secret_string)["confluent_cloud_secret"]
 }
 
 resource "confluentcloud_environment" "environment" {
@@ -66,10 +87,10 @@ resource "confluentcloud_kafka_cluster" "dev" {
   region           = "ap-southeast-2"
   availability     = "LOW"
   environment_id   = confluentcloud_environment.environment.id
-  deployment       = {
-                       "sku" = "BASIC"
-                     }
-  network_egress   = 100
-  network_ingress  = 100
-  storage          = 5000
+  deployment = {
+    "sku" = "BASIC"
+  }
+  network_egress  = 100
+  network_ingress = 100
+  storage         = 5000
 }
