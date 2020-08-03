812c4b334c3167e0945144119697d283dd9d2393
Sougata Khan <sougat818@gmail.com>
2020-08-03 05:33:09 +0000

    Formatting

--- terraform-state-file-backend/main.tf
+++ terraform-state-file-backend/main.tf
@@ -1,23 +1,23 @@
 provider "aws" {
-  region = "ap-southeast-2"
+  region  = "ap-southeast-2"
   version = "~> 3.0"
 }
 
 variable "default_tags" {
   default = {
-    Cost = "Confluent"
+    Cost    = "Confluent"
     Project = "Confluent Cloud Kafka Spring Boot"
   }
   description = "default resource tags"
-  type = map(string)
+  type        = map(string)
 }
 
 resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  block_public_acls = true
-  block_public_policy = true
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
   restrict_public_buckets = true
-  ignore_public_acls = true
+  ignore_public_acls      = true
 }
 
 resource "aws_s3_bucket" "terraform_state" {
@@ -33,10 +33,10 @@ resource "aws_s3_bucket" "terraform_state" {
     }
   }
   tags = merge(
-  var.default_tags,
-  {
-    Env = "dev"
-  },
+    var.default_tags,
+    {
+      Env = "dev"
+    },
   )
 
   # This import limitation causes false positives on updates required. Ignoring these fields for now.
@@ -50,18 +50,18 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name = "github-sougat818-confluent-cloud-aws-terraform-state-locks"
+  name         = "github-sougat818-confluent-cloud-aws-terraform-state-locks"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
   attribute {
     name = "LockID"
     type = "S"
   }
   tags = merge(
-  var.default_tags,
-  {
-    Env = "dev"
-  },
+    var.default_tags,
+    {
+      Env = "dev"
+    },
   )
 }
 
@@ -71,13 +71,13 @@ resource "aws_dynamodb_table" "terraform_locks" {
 //    "confluent_cloud_secret": "XXXX"
 //  }
 resource "aws_secretsmanager_secret" "confluent_cloud" {
-  name = "github-sougat818-confluent-cloud-admin"
+  name                    = "github-sougat818-confluent-cloud-admin"
   recovery_window_in_days = 0
-  description = "key and secret for confluent cloud with cluster creation access"
+  description             = "key and secret for confluent cloud with cluster creation access"
   tags = merge(
-  var.default_tags,
-  {
-    Env = "dev"
-  },
+    var.default_tags,
+    {
+      Env = "dev"
+    },
   )
 }
